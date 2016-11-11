d044a0a1a99294ce5af6c327f5d4b5035fef2d61
javierjcf <javierjcf@gmail.com>
2016-11-11 12:39:56 +0100

    [ADD]umai_custom_documents

---
+++ project-addons/umai_custom_documents/README.rst
@@ -0,0 +1,24 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==================
+CMNT Custom Reports
+==================
+
+Customizations for UMAI. Depends on cmnt_custom_reports and removes taxes table
+and variable field taxes in invoice_report. Set instead, IVA 10%.
+
+
+Credits
+=======
+
+Contributors
+------------
+* Comunitea
+* Javier Colmenero <javier@comunitea.com>
+
+Maintainer
+----------
+
+This module is maintained by the Comunitea <http://www.comunitea.comm>.
---
+++ project-addons/umai_custom_documents/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'UMAI Custom Documents',
+    'version': '8.0.0.0.0',
+    'author': 'Comunitea ',
+    "category": "Custom",
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'cmnt_custom_reports',
+    ],
+    'contributors': [
+        "Comunitea ",
+        "Javier Colmenero <javier@comunitea.com>"
+    ],
+    "data": [
+        "views/report_invoice.xml",
+    ],
+    "demo": [
+    ],
+    'test': [
+    ],
+    "installable": True
+}
