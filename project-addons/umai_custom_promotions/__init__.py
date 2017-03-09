51b75ef9a1109b68218a38df75a0b57f17ad48bc
javierjcf <javierjcf@gmail.com>
2017-03-09 12:56:51 +0100

    [ADD]umai_custom_promotions,umai_custom_documents: Post init hook

--- project-addons/umai_custom_promotions/__init__.py
+++ project-addons/umai_custom_promotions/__init__.py
@@ -3,4 +3,16 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 import models
-import hooks
+
+
+def copy_discount_column(cr, registry):
+    """This post-init-hook copy update all discount columns to
+    the new discount 1"""
+    cr.execute("""
+        UPDATE account_invoice_line
+        SET discount1 = discount
+        """)
+    cr.execute("""
+        UPDATE sale_order_line
+        SET discount1 = discount
+        """)
