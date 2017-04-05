23ee3f995e8b77ad136533037e6296ff6b069628
javierjcf <javierjcf@gmail.com>
2017-04-05 14:33:09 +0200

    [ADD]sale_custom: Líneas de pedido editables en vista tree

---
+++ project-addons/sale_custom/README.rst
@@ -0,0 +1,25 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=====================
+Sale Custom
+=====================
+
+Customizations over sales.
+
+* Make Sale order line editable in tree view
+
+
+Credits
+=======
+
+Contributors
+------------
+* Comunitea
+* Javier Colmenero <javier@comunitea.com>
+
+Maintainer
+----------
+
+This module is maintained by the Comunitea <http://www.comunitea.comm>.
---
+++ project-addons/sale_custom/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Sale Custom',
+    'version': '8.0.0.0.0',
+    'author': 'Comunitea ',
+    "category": "Custom",
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'product',
+        'sale',
+    ],
+    'contributors': [
+        "Comunitea ",
+        "Javier Colmenero <javier@comunitea.com>"
+    ],
+    "data": [
+        "views/sale_view.xml",
+    ],
+    "demo": [
+    ],
+    'test': [
+    ],
+    "installable": True
+}
